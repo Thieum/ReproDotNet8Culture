0faedb3c23a55daa8119887b3905e0ef573b7abc
Matthieu Penant <matthieu.penant@seika.ca>
2023-12-13 13:59:07 -0500

    Add project files.

---
+++ ReproDotNet8Culture/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ReproDotNet8Culture
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToString("d", System.Globalization.CultureInfo.CreateSpecificCulture("en-CA")));
+        }
+    }
+}
